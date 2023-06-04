92dfdf285a160b9a5c29a8e378360129ce0675eb
ADM <dimahudobin05@gmail.com>
2023-06-05 01:15:20 +0700

    админ панель

--- locallibrary/locallibrary/catalog/admin.py
+++ locallibrary/locallibrary/catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Book, BookInstance, Author, Genre
 
-# Register your models here.
+# Register your models her
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
